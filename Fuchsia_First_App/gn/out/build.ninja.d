bdb532a2ca12d6c90d4bebf4430efb7b55213c12
Mr Zhao <314236413@qq.com>
2020-03-16 18:53:28 +0800

    使用Ninja手写Fuchsia第一个app

---
+++ Fuchsia_First_App/gn/out/build.ninja.d
@@ -0,0 +1,1 @@
+build.ninja: ../build/gen.py ../build/build_linux.ninja.template
---
+++ Fuchsia_First_App/myApp/out/default/build.ninja.d
@@ -0,0 +1,1 @@
+build.ninja: ../../.gn ../../BUILD.gn ../../build/BUILD.gn ../../build/BUILDCONFIG.gn ../../build/toolchain/BUILD.gn ./args.gn
